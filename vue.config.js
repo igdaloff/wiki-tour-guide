c8f68b6a6bf154b3fcfe1e049ba2f5c9debd7ae4
Nathan Igdaloff <louis.nathan@gmail.com>
2022-10-20 17:12:27 -0700

    install and setup vue pwa plugin

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,25 @@ module.exports = defineConfig({
     loaderOptions: {
       sass: {
         additionalData: `@import "@/assets/global.scss";`,
-      }
-    }
+      },
+    },
   },
-});
+  pwa: {
+    name: 'WWikipedia Walking Guide',
+    display: 'standalone',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: '#222222',
+    favicon32: 'img/icons/72.png',
+    favicon16: 'img/icons/48.png',
+    appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+    assetVersion: 2,
+    manifestOptions: {
+      start_url: '/',
+      background_color: '#222222',
+      theme_color: '#ffffff',
+      short_name: 'WWG',
+      orientation: 'portrait-primary',
+    },
+    workboxPluginMode: 'GenerateSW',
+  },
+})
