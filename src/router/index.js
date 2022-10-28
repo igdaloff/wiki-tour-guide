939058b71ff17d5bbae46d29ed4e994d440c834f
Nathan Igdaloff <louis.nathan@gmail.com>
2022-10-27 17:45:25 -0700

    add router hash mode: fix issue where refreshing /list page returns 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import PlaceList from "../views/PlaceList.vue"
+import PlaceList from '../views/PlaceList.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
     path: '/list',
     name: 'List',
-    component: PlaceList
-  }
+    component: PlaceList,
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  mode: 'hash',
 })
 
 export default router
